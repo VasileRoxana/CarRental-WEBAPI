587b4152565282a2e167e8d6c94b6df0dd7199e4
VasileRoxana <vasile.roxana.7a@gmail.com>
2020-02-05 21:59:45 +0200

    Stable Authorization service, Editting with no picture uploaded BUG

--- CarRental-WebAPI/CarRental.WebAPI/Controllers/HomeController.cs
+++ CarRental-WebAPI/CarRental.WebAPI/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CarRental.WebAPI.Controllers
 {
-   // [Authorize]
+    [Authorize]
     public class HomeController : Controller
     {
         // GET: /<controller>/
