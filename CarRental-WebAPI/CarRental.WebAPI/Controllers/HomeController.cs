c1f431fc80c769f2070b302a69b0750a529a2f6f
VasileRoxana <vasile.roxana.7a@gmail.com>
2020-02-06 00:35:01 +0200

    Edit bug still here

--- CarRental-WebAPI/CarRental.WebAPI/Controllers/HomeController.cs
+++ CarRental-WebAPI/CarRental.WebAPI/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using CarRental.Domain.Models.ViewModels;
 using CarRental.Domain.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.IO;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.AspNetCore.Hosting;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,8 +20,11 @@ namespace CarRental.WebAPI.Controllers
     {
         // GET: /<controller>/
         private readonly ICarRepository _carRepository;
-        public HomeController(ICarRepository carRepository) {
+        private readonly IHostingEnvironment hostingEnvironment;
+        public HomeController(ICarRepository carRepository, 
+                              IHostingEnvironment hostingEnvironment ) {
             _carRepository = carRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
         [AllowAnonymous]
         public ViewResult Index()
@@ -47,16 +53,31 @@ namespace CarRental.WebAPI.Controllers
 
         // Post method for creating a new car and displaying it
         [HttpPost]
-        public IActionResult Create(Car car)
+        public IActionResult Create(CarCreateViewModel model)
         {
             // check if we have valid data in the form
             if (ModelState.IsValid)
             {
-                Car newCar = _carRepository.Add(car);
+                string uniqueFileName = ProcessUploadedFile(model);
+
+                Car newCar = new Car
+                {
+                    CarName = model.CarName,
+                    VehicleClass = model.VehicleClass,
+                    Capacity = model.Capacity,
+                    CarType = model.CarType,
+                    Price = model.Price,
+                    PhotoPath = uniqueFileName
+                    // Store the file name in PhotoPath property of the car object
+                    // which gets saved to the Cars database table
+                };
+
+                _carRepository.Add(newCar);
                 return RedirectToAction("details", new { id = newCar.Id });
             }
             return View();
         }
+
         // Get method for the create button from navbar
         [HttpGet]
         public ViewResult Edit(int id)
@@ -82,25 +103,40 @@ namespace CarRental.WebAPI.Controllers
             if (ModelState.IsValid)
             {
                 Car car = _carRepository.GetCarById(model.Id);
-                Console.WriteLine(car);
-                car.Id = model.Id;
                 car.CarName = model.CarName;
                 car.VehicleClass = model.VehicleClass;
                 car.Capacity = model.Capacity;
                 car.CarType = model.CarType;
                 car.Price = model.Price;
-                if (model.PhotoPath != null)
-                {
-                    car.PhotoPath = model.PhotoPath;
-                }
-                else 
-                    car.PhotoPath = model.ExistingPhotoPath;
-                model.ExistingPhotoPath = model.PhotoPath;
 
-                _carRepository.Update(car);
+                if (model.Photo != null)
+                {
+                    //if (model.ExistingPhotoPath != null)
+                    //{
+                        string filePath = Path.Combine(hostingEnvironment.WebRootPath, "img", model.ExistingPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    //}
+                    car.PhotoPath = ProcessUploadedFile(model);
+                }
+
+                _carRepository.Add(car);
                 return RedirectToAction("index");
             }
             return View();
+        }
+
+        private string ProcessUploadedFile(CarCreateViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.Photo != null)
+            {
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "img");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                model.Photo.CopyTo(new FileStream(filePath, FileMode.CreateNew));
+            }
+
+            return uniqueFileName;
         }
     }
  }
--- CarRental-WebAPI/CarRental.WebAPI/ServicesExtensionMethods.cs
+++ CarRental-WebAPI/CarRental.WebAPI/ServicesExtensionMethods.cs
@@ -15,7 +15,7 @@ namespace CarRental.WebAPI
         public static void ConfigureDbContext(this IServiceCollection services, IConfiguration config)
         {
             //string connectionString = config["ConnectionStrings:CarRentalWebAPIContextConnection"];
-            string connectionString = config["ConnectionStrings:CarRentalDBConnection"];
+            string connectionString = config["ConnectionStrings:CarRentalDBConnectionV1.0"];
             services.AddDbContext < CarRentalDbContext>(c => c.UseSqlServer(connectionString, b => b.MigrationsAssembly("CarRental.WebAPI")));
         }
 
